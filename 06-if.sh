c4654fa624f8e0bfde3e891d56c992b6b2c0fc4e
vijayaws9775 <reddyvijaykanth24@gmail.com>
2023-01-02 19:04:14 +0530

    Catalogue script

--- 06-if.sh
+++ 06-if.sh
@@ -1,8 +1,14 @@
 a=$1
-if [ $a -le 10 ]
+if [ $a -ne 10 ]
 then
   echo $a value is lessar than 10 or equal to 10
 else
   echo $a value is greater than 10
 fi
 
+x="abc"
+
+if [ $x == "abc"]
+then
+  echo $x is equal to "abc"
+fi
