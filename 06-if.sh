14314358318d67e5cafa4d3f215085763d0b29c6
vijayaws9775 <reddyvijaykanth24@gmail.com>
2023-01-02 18:54:43 +0530

    Catalogue script

--- 06-if.sh
+++ 06-if.sh
@@ -1,8 +1,8 @@
 a=$1
-if [ $a -gt 10 ]
+if [ $a -lt 10 ]
 then
-  echo $a value is greater than 10
+  echo $a value is lessar than 10
 else
-  echo $a value is lesser than 10
+  echo $a value is greater than 10
 fi
 
